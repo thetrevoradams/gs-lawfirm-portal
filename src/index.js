f9dab5049c9d2d706524ce1d1cd7fdb6e2a22e6a
Teton Dev <trevor@teton.dev>
2021-02-27 15:29:27 -0700

    renaming file to see if it updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
-import App from './app'
+import App from './App'
 // import reportWebVitals from './reportWebVitals'
 
 Sentry.init({
