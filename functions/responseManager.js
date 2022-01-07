fafc12db6137db29c18fa620225580b899154173
Trevor <trevor@Trevors-MacBook-Air.local>
2022-01-07 01:01:52 -0700

    record notes override fix

--- functions/responseManager.js
+++ functions/responseManager.js
@@ -113,6 +113,19 @@ async function addLegalAction({ dataToken, recordId, response, date, oldActions,
   return json
 }
 
+async function latestRecord(dataToken, recordId) {
+  const dataRaw = await fetch(`${process.env.REACT_DB_URL}/_find`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${dataToken}`,
+    },
+    body: JSON.stringify({ query: [{ recordId }] }),
+  })
+  const json = await dataRaw.json()
+  return json
+}
+
 exports.handler = async (entry) => {
   const { recordId, response, type, date, oldActions, itemHistory, urgent, lawFirmData, record } = JSON.parse(
     entry.body
@@ -124,6 +137,8 @@ exports.handler = async (entry) => {
       // --- DATA TOKEN ---
       const tokenJson = await fetchToken(resp.clarisIdToken)
       const dataToken = tokenJson.response.token
+
+      const recordRefresh = await latestRecord(dataToken, recordId)
 
       if (dataToken) {
         let submissionResp = ''
@@ -137,7 +152,7 @@ exports.handler = async (entry) => {
             urgent,
             date,
             lawFirmData,
-            record,
+            record: recordRefresh || record,
           })
         } else if (type === 'legalAction') {
           submissionResp = await addLegalAction({
